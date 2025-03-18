e8bc36bba6d6e16b5dda4f5f4f8a08f5e97e1022
unknown <sachinchakradhar8@gmail.com>
2025-03-18 21:51:30 +0545

    hero container added

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Container } from "./components/container";
+import { Hero } from "./components/hero";
 
 
 export default function Home() {
@@ -8,7 +9,12 @@ export default function Home() {
         <Container>header</Container>
       </header>
       <main>
-        <Container>Linears</Container>
+        <Container>
+          <Hero
+            title="Linear is a purpose-built tool for planning and building products"
+            subtitle="Meet the system for modern software development. Streamline issues, projects, and product roadmaps."
+          />
+        </Container>
       </main>
       <footer>
         <Container>
